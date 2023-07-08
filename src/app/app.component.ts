c76c63209028a6f757957c1173316148e60e0b07
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-08 10:39:34 -0400

    feat: create and add skills section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,21 @@ import { HomeComponent } from './sections/components/home/home.component';
 import { WorksComponent } from './sections/components/works/works.component';
 import { AboutMeComponent } from './sections/components/about-me/about-me.component';
 import { ContactsComponent } from './sections/components/contacts/contacts.component';
+import { SkillsComponent } from '@sections/components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, WorksComponent, AboutMeComponent, ContactsComponent]
+  imports: [
+    HeaderComponent,
+    HomeComponent,
+    WorksComponent,
+    AboutMeComponent,
+    ContactsComponent,
+    SkillsComponent,
+  ],
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/sections/components/skills/skills.component.ts
+++ src/app/sections/components/skills/skills.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '@shared/components/card/card.component';
+import { TitleComponent } from '@shared/components/title/title.component';
 
 @Component({
   selector: 'ed-skills',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent, TitleComponent],
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
