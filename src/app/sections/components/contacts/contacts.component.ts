46d2485b2c7c6bcf6c8f0d551828c73b5822d15f
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-08 13:53:15 -0400

    feat(contacts): update content

--- src/app/sections/components/contacts/contacts.component.ts
+++ src/app/sections/components/contacts/contacts.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/components/title/title.component';
+import { IconComponent } from '@shared/components/icon/icon.component';
 
 @Component({
   selector: 'ed-contacts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, IconComponent],
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
