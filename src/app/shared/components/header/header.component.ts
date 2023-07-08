a4aeecffbabb01a9395d7303caea62ee9b30c88c
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-08 14:30:30 -0400

    feat(header): use our new logo component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { IconComponent } from '../icon/icon.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BurgerMenuComponent } from '../burger-menu/burger-menu.component';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'ed-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, IconComponent, BurgerMenuComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    IconComponent,
+    BurgerMenuComponent,
+    LogoComponent,
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
-}
+export class HeaderComponent {}
