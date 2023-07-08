9fc324e8a732549ed48cb3197eeb4bd40fbb6ec4
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-08 13:26:59 -0400

    feat(about-me-section): update content

--- src/app/sections/components/about-me/about-me.component.ts
+++ src/app/sections/components/about-me/about-me.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/components/title/title.component';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { FigureMatrixComponent } from '@shared/components/figure-matrix/figure-matrix.component';
 
 @Component({
   selector: 'ed-about-me',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, ButtonComponent, FigureMatrixComponent],
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
