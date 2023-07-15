6770f5fc21339f33f3250bcb7c180be757d306b2
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-15 15:55:43 -0400

    feat(header): mimic the scrolling behavior of material app bars

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../icon/icon.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -20,4 +26,36 @@ import { LogoComponent } from '../logo/logo.component';
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  scrollOffsetToHideHeader = 40;
+  currentTimeoutId: ReturnType<typeof setTimeout> | null = null;
+  lastScrollY = 0;
+
+  @HostBinding('style.top')
+  top = '0';
+
+  /**
+   * We tried to mimic the behavior of materail app bar scrolling
+   * check out {@link https://m2.material.io/components/app-bars-top#behavior}
+   */
+  @HostListener('window:scroll')
+  handleWindowScroll() {
+    if (this.currentTimeoutId) {
+      clearTimeout(this.currentTimeoutId);
+    }
+    this.currentTimeoutId = setTimeout(() => {
+      const headerHeight = this._elementRef.nativeElement.offsetHeight;
+      if (
+        window.scrollY >= this.scrollOffsetToHideHeader &&
+        this.lastScrollY - window.scrollY <= 0
+      ) {
+        this.top = `-${headerHeight}px`;
+      } else {
+        this.top = '0';
+      }
+      this.lastScrollY = window.scrollY;
+    }, 100);
+  }
+
+  constructor(private _elementRef: ElementRef<HTMLElement>) {}
+}
