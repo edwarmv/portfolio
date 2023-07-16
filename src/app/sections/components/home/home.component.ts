a743dbe09585a39cec57e4add5f8114d5b0c7faf
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-16 09:59:35 -0400

    feat(home): update content

--- src/app/sections/components/home/home.component.ts
+++ src/app/sections/components/home/home.component.ts
@@ -1,11 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoIconComponent } from '@shared/components/logo-icon/logo-icon.component';
+import { FigureMatrixComponent } from '@shared/components/figure-matrix/figure-matrix.component';
+import { IconComponent } from '@shared/components/icon/icon.component';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ed-home',
   standalone: true,
-  imports: [CommonModule, LogoIconComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    LogoIconComponent,
+    FigureMatrixComponent,
+    IconComponent,
+    ButtonComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
