77ee0584058784714901258cde59131dcbf4d745
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-17 16:56:51 -0400

    feat(footer): update content

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,15 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../logo/logo.component';
 import { IconComponent } from '../icon/icon.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'ed-footer',
   standalone: true,
-  imports: [CommonModule, LogoComponent, IconComponent],
+  imports: [CommonModule, LogoComponent, IconComponent, ButtonComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
